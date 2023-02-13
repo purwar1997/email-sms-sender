ee3add770824444b97567e1c6c2aea1b5b815a46
purwar1997 <shubhampurwar511@gmail.com>
2023-02-13 23:55:34 +0530

    frontend finished

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,8 +7,6 @@ const config = {
   SMTP_USERNAME: process.env.SMTP_USERNAME,
   SMTP_PASSWORD: process.env.SMTP_PASSWORD,
   SMTP_SENDER_EMAIL: process.env.SMTP_SENDER_EMAIL,
-  INFOBIP_API_KEY: process.env.INFOBIP_API_KEY,
-  INFOBIP_API_BASE_URL: process.env.INFOBIP_API_BASE_URL,
 };
 
 export default config;
